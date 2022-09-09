86b427408a2a1b4854d7adfa6eef7cd989891028
SHALOM PEREIRA DOS SANTOS <202002547502@alunos.estacio.br>
2022-09-08 21:06:25 -0300

    photo pronta

--- App.js
+++ App.js
@@ -1,26 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import dedinho from './assets/dedinho.gif';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.texto}>Hellow World</Text>
-      <StatusBar style="auto" />
-    </View>
-    
+    <>
+      <View style={styles.container}>
+        <Image source={dedinho} style={{width: 300, height: 300,}} ></Image>
+        <Text style={styles.texto}>Dedinho</Text>
+        <Text style={styles.texto}>pra cima</Text>
+        <StatusBar style="auto" />
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFE376',
     alignItems: 'center',
     justifyContent: 'center',
   },
   texto: {
     backgroundColor: 'grey',
     padding: '50px',
+    fontSize: '40px',
+    color: 'white',
     borderRadius: '20px',
   },
+  photo:{
+
+  }
 });
