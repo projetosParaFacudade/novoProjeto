c4dfeb689660bf184387f43e02199b83d6e36a5a
shalomsantos <shalomsantos1234@gmail.com>
2022-10-13 17:45:03 -0300

    quase pronto

--- Screens/Details.js
+++ Screens/Details.js
@@ -1,13 +1,47 @@
 
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Button, StyleSheet} from 'react-native';
 
 export default function Details() {
   return (
     <>
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-        <Text>Details</Text>
+      <View style={styles.body}>
+        <View style={styles.header}>
+          <Text style={styles.innerTextTitle}>Go work</Text>
+        </View>
+        <View style={styles.buttonLocale}>
+          <Button
+            title="Back"
+            onPress={() => navigation.navigate('Home')} />
+        </View>
       </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#66C3E2',
+  },
+  header: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#66C3E2',
+  },
+  buttonLocale:{
+    height: '400px',
+    padding: '30px',
+    borderTopLeftRadius: '50px',
+    borderTopRightRadius: '50px',
+    backgroundColor: 'white',
+  },
+  innerTextTitle:{
+    color: 'white',
+    fontSize: '60px'
+  },
+  innerText:{
+    color: 'white',
+    fontSize: '30px'
+  }
+});
--- Screens/Home.js
+++ Screens/Home.js
@@ -1,15 +1,53 @@
 import React from 'react';
-import { View, Text, Button } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
+import profile from '../assets/profile.png';
+
 
 export default function Home( {navigation} ) {
   return (
     <>
-      <Text>Home</Text>
-      <View style={{ alignItems: 'center', justifyContent: 'center' }}>
-        <Button
-          title="Details"
-          onPress={() => navigation.navigate('Details')} />
+      <View style={styles.body}>
+        <View style={styles.header}>
+          <Text style={styles.innerTextTitle}>Welcome To</Text>
+          <Text style={styles.innerText}>your workspace</Text>
+        </View>
+        <View style={styles.header}>
+          <Image style={{ width: '150px', height: '150px' }}
+            source={profile} />
+        </View>
+        <View style={styles.buttonLocale}>
+          <Button
+            title="Entrar"
+            onPress={() => navigation.navigate('Details')} />
+        </View>
       </View>
     </>
   );
 }
+const styles = StyleSheet.create({
+  body: {
+    flex: 1,
+    backgroundColor: '#66C3E2',
+  },
+  header: {
+    flex: 2,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#66C3E2',
+  },
+  buttonLocale:{
+    height: '300px',
+    padding: '50px',
+    borderTopLeftRadius: '40px',
+    borderTopRightRadius: '40px',
+    backgroundColor: 'white',
+  },
+  innerTextTitle:{
+    color: 'white',
+    fontSize: '60px'
+  },
+  innerText:{
+    color: 'white',
+    fontSize: '30px'
+  }
+});
